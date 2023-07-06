eb41200683cf15cd236aa6d369dd405349fdcd58
Isobel Ibironke <isobel.ibironke@softwire.com>
2023-07-06 09:35:38 +0100

    knights move in L-shape

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,25 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var moveSquares = new List<Square>();
+
+            int[] dx = { 1, 1, -1, -1 };
+            int[] dy = { 1, -1, 1, -1 };
+
+            for (var i = 0; i < 4; i++)
+            {
+                var newRow = currentSquare.Row + dx[i];
+                var newCol = currentSquare.Col + 2 * dy[i];
+                if (newRow >= 0 && newRow < GameSettings.BoardSize && newCol >= 0 && newCol < GameSettings.BoardSize)
+                {
+                    moveSquares.Add(Square.At(newRow,newCol));
+                    (newRow, newCol) = (newCol, newRow);
+                    moveSquares.Add(Square.At(newRow,newCol));
+                }
+            }
+
+            return moveSquares;
         }
     }
 }
